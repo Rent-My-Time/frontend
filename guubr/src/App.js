04a308660ffa89f791be3edfc68ca3e4f8cc9b55
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-07-19 21:16:53 -0600

    fixes routing for home and profile page

--- guubr/src/App.js
+++ guubr/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile-page" exact component={ProfilePage} />
+      </Switch>
+    </Router>
   );
- }
- export default App;
+}
+export default App;

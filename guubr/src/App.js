3407410b77dc05ce7e5b6616a0791343be4446a0
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-07-19 21:40:50 -0600

    fixes routing for signup and login

--- guubr/src/App.js
+++ guubr/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
+import AccountBox from "./components/userAccountBox/index";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={AccountBox} />
         <Route path="/profile-page" exact component={ProfilePage} />
       </Switch>
     </Router>

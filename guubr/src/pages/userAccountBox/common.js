eff70366c964e93d6680357b01327a6615de1b28
Gianna Pena <giannimpe1@yahoo.com>
2021-07-23 17:22:04 -0400

    Finish Animation, Add signUpForm

--- guubr/src/pages/userAccountBox/common.js
+++ guubr/src/pages/userAccountBox/common.js
@@ -8,13 +8,15 @@ export const BoxContainer= styled.div`
      flex-direction: column;
      align-items:center;
      margin-top: 10px;
-
 `;
 
 export const FormContainer=styled.form`
      width: 100%;
+     margin: 10px;
      display: flex;
      flex-direction: column;
+     box-shadow: 0px, 0px, 2.5px rgba(15, 15, 15 , 0.19);
+     padding-left: 10px;
 `;
 
 // **** Used as Hyperlink when you forgot your password
@@ -28,14 +30,15 @@ export const MutedLink = styled.a`
 
 export const BoldLink = styled.a`
      font-size: 12px;
-     color: rgba( 126, 136, 195 , 100 );
+     color: #7263e3;
      font-weight: 500;
      text-decoration: none;
+     margin: 0 4px;
 
 `;
 
 export const Input = styled.input`
-  width: 100%;
+  width: 90%;
   height: 42px;
   outline: none;
   border: 1px solid rgba(200, 200, 200, 0.3);
@@ -43,32 +46,52 @@ export const Input = styled.input`
   border-bottom: 1.4px solid transparent;
   transition: all 200ms ease-in-out;
   font-size: 12px;
+
   &::placeholder {
-    color: rgba(200, 200, 200, 1);
+    color: rgba(200, 200, 200, 1
+      );
   }
+
   &:not(:last-of-type) {
     border-bottom: 1.5px solid rgba(200, 200, 200, 0.4);
   }
+
   &:focus {
     outline: none;
-    border-bottom: 2px solid  rgba( 126, 136, 195 , 100 );
+    border-bottom: 2px solid rgb(136, 142, 176, 0.4);
   }
 `;
 
-
 export const SubmitButton = styled.button`
-  width: 100%;
-  padding: 11px 40%;
+  width: 90%;
+  margin: 5px;
+  padding: 11px 20px;
+  display: inline-block;
   color: #fff;
   font-size: 15px;
   font-weight: 600;
+  text-align: center;
   border: none;
-  border-radius: 100px 100px 100px 100px;
+  border-radius: 3px 3px 3px 3px;
   cursor: pointer;
   transition: all, 240ms ease-in-out;
-  background: rgba(148, 148, 150, 1);
+  background: rgba(138, 146, 180, 1);
+
 
     &:hover {
       filter: brightness(1.03);
     }
   `;
+
+const BookingPage__Logo =styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 0 2%;
+`
+const BookingPage__Header =styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 3%;
+`
